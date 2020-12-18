c1d24b3375da1d7c84a2c0ecdb83bd644ebfab13
Marek Kubiak <marekkubiak@Mareks-MacBook-Pro.local>
2020-12-18 21:37:10 +0000

    created airpollution app and created models for the app

---
+++ airpollution/models.py
@@ -0,0 +1,52 @@
+from django.db import models
+
+
+class Pollutant(models.Model):
+    """Pollution model for airpollution app"""
+    name = models.CharField(max_length=10, primary_key=True)
+    removed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'pollutants'
+
+
+class Country(models.Model):
+    """Country model for airpollution app"""
+    name = models.CharField(max_length=100, unique=True)
+    removed = models.BooleanField(default=False)
+    longitude = models.FloatField(null=True)
+    latitude = models.FloatField(null=True)
+    altitude = models.FloatField(null=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'countries'
+
+
+class PollutantEntry(models.Model):
+    """PolutantEntry model for airpollution app"""
+    pollutant = models.ForeignKey(Pollutant, on_delete=models.CASCADE, related_name='entries')
+    country = models.ForeignKey(Country, on_delete=models.CASCADE, related_name='entries')
+    year = models.IntegerField()
+    city = models.CharField(max_length=50, default='', blank=True)
+    station_code = models.CharField(max_length=20, default='', blank=True)
+    station_name = models.CharField(max_length=100, default='', blank=True)
+    pollution_level = models.IntegerField()
+    units = models.CharField(max_length=10, default='', blank=True)
+    station_type = models.CharField(max_length=20, default='', blank=True)
+    station_area = models.CharField(max_length=20, default='', blank=True)
+    longitude = models.FloatField(null=True)
+    latitude = models.FloatField(null=True)
+    altitude = models.FloatField(null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.id} {self.pollutant.name} {self.year}'
+
+    class Meta:
+        verbose_name_plural = 'pollutant entries'
--- mareks_portfolio/settings.py
+++ mareks_portfolio/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'airpollution',
     'website',
 ]
 
@@ -124,3 +125,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = BASE_DIR / 'media'
