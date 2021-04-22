232919c7d05f714da01bf4fe5fd9dfc1440853ec
Marek Kubiak <marekkubiak@Mareks-MacBook-Pro.local>
2021-04-22 20:41:23 +0100

    made first class based views for Income model

--- mareks_portfolio/urls.py
+++ mareks_portfolio/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('website.urls')),
     path('airpollution/', include('airpollution.urls')),
+    path('my_finances/', include('my_finances.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- my_finances/views.py
+++ my_finances/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
+from django.views.generic.list import ListView
 
-# Create your views here.
+from my_finances.models import Income
+
+
+class IncomeListView(ListView):
+    model = Income
+    paginate_by = 100
+
+
+class IncomeDetailView(DetailView):
+    model = Income
+
+
+class IncomeCreatView(CreateView):
+    model = Income
+    fields = ['value', 'date', 'type', 'repetitive', 'repetition_interval', 'repetition_time']
+    success_url = reverse_lazy('my_finances:income_list')
+
+
+class IncomeUpdateView(UpdateView):
+    model = Income
+    fields = ['value', 'date', 'type', 'repetitive', 'repetition_interval', 'repetition_time']
+    success_url = reverse_lazy('my_finances:income_list')
+
+
+class IncomeDeleteView(DeleteView):
+    model = Income
+    success_url = reverse_lazy('my_finances:income_list')
