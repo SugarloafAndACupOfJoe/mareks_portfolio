b434e97f4f7df4ed4f04366a268ff28c90ade34d
Marek Kubiak <marek.kubiak88@gmail.com>
2021-12-21 16:14:01 +0000

    added React

---
+++ frontend/src/components/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function Hello() {
+    return <h1>Hello World!</h1>
+}
+
+ReactDOM.render(<Hello />, document.getElementById('app'))
